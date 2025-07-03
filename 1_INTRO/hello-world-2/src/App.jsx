d6eefef39066d4fd734608f4f24a9533ba49a811
Eduardo Castro <eduardocastrobritoo@gmail.com>
2025-07-03 16:19:56 -0300

    init hello world

---
+++ 1_INTRO/hello-world-2/src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <>
+      <div>
+        <h1>Hello World Giovana Amor da Minha vida!!</h1>
+        <p>Testando parágrafo!</p>
+      </div>
+    </>
+  );
+}
+
+export default App;
