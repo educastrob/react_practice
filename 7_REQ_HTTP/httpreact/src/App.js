c1d03f1bb738e82abc0912cb3d7f4922da51744e
Eduardo Castro <eduardocastrobritoo@gmail.com>
2025-07-08 13:20:05 -0300

    7_REQ_HTTP -> init data

---
+++ 7_REQ_HTTP/httpreact/src/App.js
@@ -0,0 +1,19 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+	const [products, setProducts] = useState([]);
+
+	// 1 - resgatando dados
+	useEffect(async() => {
+
+	});
+
+  return (
+    <div className="App">
+      <h1>Lista de Produtos</h1>
+    </div>
+  );
+}
+
+export default App;
