c39f57fdd32b01303d0b327e8c41efa3e6c9693f
Eduardo Castro <eduardocastrobritoo@gmail.com>
2025-07-08 14:47:10 -0300

    http config added

--- 7_REQ_HTTP/httpreact/src/App.js
+++ 7_REQ_HTTP/httpreact/src/App.js
@@ -1,26 +1,32 @@
 import './App.css';
 import { useEffect, useState } from 'react';
 
+// 4 - custom hook
+import { useFetch } from './hooks/useFetch';
+
 const url = "http://localhost:3002/products";
 
 function App() {
 	const [products, setProducts] = useState([]);
 
+	// 4 - custom
+	const { data: items } = useFetch(url);
+
 	const [name, setName] = useState("");
 	const [price, setPrice] = useState("");
 
 	// 1 - resgatando dados
-	useEffect(() => {
-		async function fetchData() {
-			const response = await fetch(url);
-			const data = await response.json();
-			setProducts(data);
-		}
+	// useEffect(() => {
+	// 	async function fetchData() {
+	// 		const response = await fetch(url);
+	// 		const data = await response.json();
+	// 		setProducts(data);
+	// 	}
 
-		fetchData();
-	}, []);
+	// 	fetchData();
+	// }, []);
 
-	console.log(products)
+	// console.log(products)
 
 
 	// 2 - adicionando dados
@@ -42,15 +48,17 @@ function App() {
 
 		// 3 - carregamento dinâmico
 		const addedProduct = await res.json();
-
 		setProducts((prevProducts) => [...prevProducts, addedProduct]);
+
+		setName("");
+		setPrice("");
 	}
 
   return (
     <div className="App">
       <h1>Lista de Produtos</h1>
 	  <ul>
-		{products.map((item) => (
+		{items && items.map((item) => (
 			<li key={item.id}>
 				{item.name} - R$ {item.price}
 			</li>
